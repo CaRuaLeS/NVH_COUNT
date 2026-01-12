ff1fb739d80482858226b4ac6ee205782902eeab
cruiz <carua97@gmail.com>
2026-01-12 15:06:04 +0100

    Implement offline capability with Service Worker

--- app.js
+++ app.js
@@ -82,6 +82,18 @@ let isDeleteMode = false;
 
 // Init
 function init() {
+    // Register Service Worker
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('./service-worker.js')
+                .then(registration => {
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                }, err => {
+                    console.log('ServiceWorker registration failed: ', err);
+                });
+        });
+    }
+
     loadData();
     render(); // Initial load of Main Table
     renderStock(); // Initial load of Stock Table
